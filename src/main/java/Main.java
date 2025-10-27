66282bee958822bad654baf6cd5b1506753c89cc
PDragun <PDragun@IBS.RU>
2025-10-27 13:17:54 +0300

    init project and add File.txt

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
+            System.out.println("Take one down, pass it around");
+            System.out.println((i - 1) + " bottles of beer on the wall!\n");
+        }
+        System.out.println("If that one bottle should happen to fall, what a waste of alcohol!");
+
+        System.out.println("No more bottles of beer on the wall, no more bottles of beer");
+        System.out.println("We've taken them down and passed them around; now we're drunk and passed out!");
+    }
+}
